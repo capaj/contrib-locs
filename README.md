fa87b35f58d35a20ef17a1046cd78e8a288279c8
Jiří Špác <capajj@gmail.com>
2020-08-25 03:18:18 +0200

    fix bad bin

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 a CLI util to keep a list of all people who are to blame for all the the inidividual lines in the codebase
 
+## Install
+
+```
+npm i contrib-locs -g
+```
+
 it stores the ledger inside the repo in json format. It can look like this:
 
 ```json
@@ -25,3 +31,19 @@ There are two command
 
 - `init`
 - `preCommit`
+
+## First run
+
+invoke `contrib-locs init` in your project repo root.
+
+## Set up githooks
+
+Here's an example how you can set up git hook to update contributors before each commit:
+
+```
+  "husky": {
+    "hooks": {
+      "pre-commit": "contrib-locs c"
+    }
+  }
+```
---
+++ src/bin/contrib-locs.ts
@@ -0,0 +1,98 @@
+import { install as installSourceMapSupport } from 'source-map-support'
+import git, { Commit } from 'nodegit'
+import path from 'path'
+import micromatch from 'micromatch'
+import debug from 'debug'
+import Yargs from 'yargs'
+import { LocsStatsPerUser } from '../LocsStats'
+import { onPreCommit } from '../onPreCommit'
+installSourceMapSupport()
+const log = debug('contrib-locs')
+
+const getConfig = async () => {
+  return {
+    match: ['*.ts', '*.js', '*.md']
+  }
+}
+
+Yargs.scriptName('contrib-locs')
+  .command(
+    ['init', 'i'],
+    'initialize a contributors ledger in this repo',
+    {
+      path: {
+        alias: 'p',
+        default: './.git'
+      }
+    },
+    async ({ path: gitPath }) => {
+      const statInstance = new LocsStatsPerUser(false)
+      const config = await getConfig()
+      const repo = await git.Repository.open(path.resolve(__dirname, gitPath))
+      const firstCommitOnMaster = await repo.getMasterCommit()
+      const revwalk = git.Revwalk.create(repo)
+      revwalk.reset()
+      revwalk.sorting(git.Revwalk.SORT.TIME, git.Revwalk.SORT.REVERSE)
+      revwalk.push(firstCommitOnMaster.id())
+
+      // step through all OIDs for the given reference
+      const allOids = []
+      let hasNext = true
+      let commit: Commit | undefined
+      while (hasNext) {
+        try {
+          const oid = await revwalk.next()
+          log(`commit: ${oid.tostrS()}`)
+          commit = await repo.getCommit(oid)
+          const authorEmail = commit.author().email()
+
+          const diff = await commit.getDiff()
+          await Promise.all(
+            diff.map(async (d) => {
+              const patches = await d.patches()
+
+              await Promise.all(
+                patches.map(async (patch) => {
+                  const path = patch.newFile().path()
+                  if (micromatch.isMatch(path, config.match)) {
+                    log(`adding a diff for ${path}: `)
+
+                    console.log(patch.lineStats())
+                    statInstance.addLineStat(authorEmail, patch.lineStats())
+                  }
+                })
+              )
+            })
+          )
+
+          allOids.push(oid)
+        } catch (err) {
+          hasNext = false
+          if (!commit) {
+            throw err
+          } else {
+            statInstance.setLastCommit(commit)
+          }
+        }
+      }
+      statInstance.countPercentages()
+
+      log(statInstance.output)
+      statInstance.saveAsFile()
+    }
+  )
+
+  .demandCommand(1)
+  .command(
+    ['preCommit', 'c'],
+    'update contributors ledger and stage it for commit',
+    {
+      path: {
+        alias: 'p',
+        default: './.git'
+      }
+    },
+    onPreCommit
+  )
+  .help()
+  .parse()
